630026fd3e667f3a6140b702e5549e709b254fde
Eymeric Chenavier <eymeric.chenavier@iut-valence.fr>
2015-04-02 12:35:35 +0200

    java doc

--- src/fr/iutvalence/pandemonium/labyrinth/Dalle.java
+++ src/fr/iutvalence/pandemonium/labyrinth/Dalle.java
@@ -2,24 +2,44 @@ package fr.iutvalence.pandemonium.labyrinth;
 
 /**
  * 
- * @author chenavie Une dalle accéssible. Le joueur peut accéder à une Dalle.
+ * @author chenavie Une dalle accéssible. Le joueur peut accéder à une Dalle, on
+ *         définit pour cette classe que aucune Dalle n'est un point de départ
+ *         ni d'arrivée.
  */
 public abstract class Dalle extends Case
 	{
+		/**
+		 * Une dalle est accessible.
+		 */
 		private static boolean accès = true;
-
+		
+		/**
+		 * une Dalle n'est pas un point de départ
+		 * 
+		 */
 		private final boolean estUnDépart = false;
-
+		
+		/**
+		 * Une dalle n'est pas un point de départ
+		 */
 		private static boolean estUneArrivée = false;
 
+		/**
+		 * Toute les Dalles sont accessible.
+		 * @return true
+		 */
 		public boolean estAcces()
 			{
 				return true;
 			}
+		/**
+		 * Un départ pourra être 
+		 * @return estUnDépart;
+		 */
 
 		public boolean estUnDépart()
 			{
-				return false;
+				return this.estUnDépart;
 			}
 
 		public boolean estUneArrivée()
